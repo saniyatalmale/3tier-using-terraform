8306328b72aac907f243fa440cc6acf98daf5981
Saniya Talmale <98592234+saniyatalmale@users.noreply.github.com>
2025-08-06 15:03:27 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,20 +1,34 @@
+# resource "aws_db_subnet_group" "db_subnet_group" {
+#   name       = "db-subnet-group"
+#   subnet_ids = [aws_subnet.private_db.id]
+#   tags = { Name = "RDS Subnet Group" }
+# }
+
 resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "db-subnet-group"
-  subnet_ids = [aws_subnet.private_db.id]
-  tags = { Name = "RDS Subnet Group" }
+  subnet_ids = [
+    aws_subnet.private_tomcat.id,
+    aws_subnet.private_db.id
+  ]
+
+  tags = {
+    Name = "RDS Subnet Group"
+  }
 }
+
 
 resource "aws_db_instance" "rds_mysql" {
   allocated_storage      = 20
   engine                 = "mysql"
-  engine_version         = "8.0"
-  instance_class         = "db.t2.micro"
-  name                   = "studentapp"
+  engine_version         = "5.7.44"                # ✅ stable version
+  instance_class         = "db.t3.micro"           # ✅ recommended over t2.micro
   username               = "admin"
   password               = "Passwd123$"
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   publicly_accessible    = false
   skip_final_snapshot    = true
-  tags = { Name = "Student-DB" }
+  tags = {
+    Name = "Student-DB"
+  }
 }
