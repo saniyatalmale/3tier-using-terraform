c9948831bf0c5165ebdf5d2a295dab90df824b7b
Saniya Talmale <98592234+saniyatalmale@users.noreply.github.com>
2025-08-06 13:11:16 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block = "192.168.0.0/16"
+  cidr_block = var.vpc_cidr
   tags = { Name = "VPC-3-tier" }
 }
 
@@ -8,25 +8,38 @@ resource "aws_internet_gateway" "igw" {
   tags = { Name = "IGW-3-tier" }
 }
 
-# Define subnets: public (nginx), private (tomcat/db)
 resource "aws_subnet" "public_nginx" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "192.168.1.0/24"
+  cidr_block              = var.public_subnet_nginx
   map_public_ip_on_launch = true
-  availability_zone       = "ap-south-1a"
+  availability_zone       = var.availability_zone
   tags = { Name = "Public-Subnet-Nginx" }
 }
 
 resource "aws_subnet" "private_tomcat" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "192.168.2.0/24"
-  availability_zone = "ap-south-1a"
+  cidr_block        = var.private_subnet_tomcat
+  availability_zone = var.availability_zone
   tags = { Name = "Private-Subnet-Tomcat" }
 }
 
 resource "aws_subnet" "private_db" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "192.168.3.0/24"
-  availability_zone = "ap-south-1a"
+  cidr_block        = var.private_subnet_db
+  availability_zone = var.availability_zone
   tags = { Name = "Private-Subnet-DB" }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "RT-Public-Subnet" }
+}
+
+resource "aws_route_table_association" "assoc_public" {
+  subnet_id      = aws_subnet.public_nginx.id
+  route_table_id = aws_route_table.public_rt.id
 }
