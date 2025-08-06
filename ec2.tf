67727ccc20d38c8ab87be53b5d7953002d975c54
Saniya Talmale <98592234+saniyatalmale@users.noreply.github.com>
2025-08-06 13:12:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,18 @@
 resource "aws_instance" "nginx" {
-  ami           = "<AMI-ID>"
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
   subnet_id     = aws_subnet.public_nginx.id
-  key_name      = "<key-pair>"
-  security_groups = [aws_security_group.nginx_sg.name]
-  tags = { Name = "Nginx-Server-Public" }
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+  associate_public_ip_address = true
+  tags = { Name = "Nginx-Server" }
 }
 
 resource "aws_instance" "tomcat" {
-  ami           = "<AMI-ID>"
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
   subnet_id     = aws_subnet.private_tomcat.id
-  key_name      = "<key-pair>"
-  security_groups = [aws_security_group.tomcat_sg.name]
-  tags = { Name = "Tomcat-Server-Private" }
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.tomcat_sg.id]
+  tags = { Name = "Tomcat-Server" }
 }
