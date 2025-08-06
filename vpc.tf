3a3689b0bbac6ccc1967f09bf5adcc22e1e819a6
Saniya Talmale <98592234+saniyatalmale@users.noreply.github.com>
2025-08-06 15:01:50 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -11,24 +11,25 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public_nginx" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_nginx
+  availability_zone       = "ap-southeast-1a"
   map_public_ip_on_launch = true
-  availability_zone       = var.availability_zone
-  tags = { Name = "Public-Subnet-Nginx" }
+  tags = { Name = "Public NGINX Subnet" }
 }
 
 resource "aws_subnet" "private_tomcat" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_tomcat
-  availability_zone = var.availability_zone
-  tags = { Name = "Private-Subnet-Tomcat" }
+  availability_zone = "ap-southeast-1b"
+  tags = { Name = "Private Tomcat Subnet" }
 }
 
 resource "aws_subnet" "private_db" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_db
-  availability_zone = var.availability_zone
-  tags = { Name = "Private-Subnet-DB" }
+  availability_zone = "ap-southeast-1c"
+  tags = { Name = "Private DB Subnet" }
 }
+
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
